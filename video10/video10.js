3d922bb7367cee768ecfadc73662b4ae4dbf62a4
Thanh Hai <haiduong.010102@gmail.com>
2024-04-20 15:26:31 +0700

    chapter2 #14

---
+++ video10/video10.js
@@ -0,0 +1,12 @@
+console.log('Hello word from HTML');
+
+let a = 5; // x <- y
+
+let b = '5';
+
+console.log(' compare a == b: ', 'a =', a, 'b =', b, 'result:', a == b)
+
+console.log(' compare a === b: ', 'a =', a, 'b =', b, 'result:', a === b)
+// === : type; value
+
+// == : value
---
+++ video9/video9.js
@@ -0,0 +1,28 @@
+console.log('Hello word from HTML')
+
+//key: value
+let a = {
+    name: 'Gwen',
+    age: '22',
+};
+
+console.log(' check data obj:', a)
+
+console.log('My name:', a.name)
+
+console.log('My age:', a.age)
+
+
+// array = [ item1, item2, item3]
+let b = ['MU', 'Chelsea', 'Liver'];
+
+console.log('typeof b:', typeof b, b)
+
+console.log('first item:', b[0], ' second item:', b[1])
+
+let family = [
+    { name: 'Gwen', age: '22' },
+    { name: 'Gwenyt', age: '23' }
+]
+
+console.log('my family:', family)
